f85b3cc38825bf5795b6213bb06ec5f99c3cd6d7
breed <bcr33d@gmail.com>
2020-05-31 10:41:59 -0700

    code cleanup and doc

--- lib/main.dart
+++ lib/main.dart
@@ -8,29 +8,53 @@ import 'package:android_intent/android_intent.dart';
 import 'dart:async';
 import 'dart:io';
 
-void scheduleAlarm(int seconds, String message) async {
-  var intent =
-      AndroidIntent(action: "android.intent.action.SET_TIMER", arguments: {
-    "android.intent.extra.alarm.LENGTH": seconds,
-    "android.intent.extra.alarm.MESSAGE": message,
-    "android.intent.extra.alarm.SKIP_UI": true
-  });
-  await intent.launch();
-}
+/**
+ * a simple app to help with managing the steps of a recipe. it is
+ * targeted at recipes with multiple steps and multiple different timers.
+ *
+ * recipes are written in markdown with a special added syntax:
+ *    any lines that start with => will be interpreted as a recipe step
+ *    with the format time_in_minutes followed by a description. for example,
+ *    => 60 mix flour, water and milk, let sit
+ *    will be interpreted as a step that has a 60 minute timer associated with
+ *    it and the description "mix flour, water and milk, let sit"
+ *
+ * a "bakes" sqllite DB holds the state of instances of the recipe. each
+ * instance is uniquely identified by the time the recipe was started. the
+ * current schema is:
+ *    ts INTEGER PRIMARY KEY - this is time in milliseconds it simply
+ *                             corresponds to when an event happened
+ *    start INTEGER - when the bake started (the recipe began). it uniquely
+ *                    identifies an instance of the recipe
+ *    recipe TEXT - the name of the recipe. it should correspond to the recipe
+ *                  markdown name without the .md extension
+ *    step INTEGER - the step that this entry corresponds to. -1 means the
+ *                   recipe itself (either the start or the end)
+ *    state INTEGER - shoud be either START_STATE or FINISH_STATE
+ */
 
-void main() {
-  runApp(SousChefApp());
-}
+/**
+ * quick and dirty alarm scheduler. i'm not sure how to do this for iOS...
+ */
+void scheduleAlarm(int seconds, String message) async =>
+    await AndroidIntent(action: "android.intent.action.SET_TIMER", arguments: {
+      "android.intent.extra.alarm.LENGTH": seconds,
+      "android.intent.extra.alarm.MESSAGE": message,
+      "android.intent.extra.alarm.SKIP_UI": true
+    }).launch();
+
+void main() => runApp(SousChefApp());
 
 class SousChefApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'sourdough bread (a couple cooks)',
+        title: 'sous chef',
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: RecipeSteps(title: 'sourdough bread (a couple cooks)'),
+        // hardcoded for now, but hopefully there will be more in the future
+        home: RecipeSteps(title: 'sourdough'),
       );
 }
 
@@ -40,7 +64,7 @@ class RecipeSteps extends StatefulWidget {
   final String title;
 
   @override
-  _RecipeStepsState createState() => _RecipeStepsState();
+  _RecipeStepsState createState() => _RecipeStepsState(title);
 }
 
 String formatDuration(Duration duration) =>
@@ -51,41 +75,33 @@ String formatDateTime(DateTime dateTime) => dateTime.year == DateTime.now().year
     : dateTime.toString().split(".").first;
 
 class Step {
-  Step(this.recipe, this.index, this.prev, this.description, this.time);
+  Step(this.recipe, this.index, this.description, this.time);
 
   final Recipe recipe;
-  final int index;
-  final Step prev;
-  Step next;
-  final String description;
-  final Duration time;
-  bool alarmScheduled = false;
-  Duration pausedTime = Duration(seconds: 0);
-
-  Duration get runTime => started == null
-      ? Duration(seconds: 0)
-      : DateTime.now().difference(started) - pausedTime;
-
-  Duration get remaining => time - runTime;
-
-  DateTime started;
+  final int index; // step number
+  final String description; // from markdown
+  final Duration time; // from markdown
+  DateTime started; // from DB (null if not set)
+  DateTime finished; // from DB (null if not set)
+  bool alarmScheduled = false; // transient (not from DB)
+  Step get prev => index == 0 ? null : recipe.steps[index - 1];
+  Step get next =>
+      index == recipe.steps.length - 1 ? null : recipe.steps[index + 1];
 
   void start() {
     started = DateTime.now();
-    recipe.logToDB(index, "", Recipe.START_STATE);
+    recipe.logToDB(index, Recipe.START_STATE);
   }
 
   void finish() {
     finished = DateTime.now();
-    recipe.logToDB(index, "", Recipe.FINISH_STATE);
+    recipe.logToDB(index, Recipe.FINISH_STATE);
     if (next != null) {
       if (next.started == null) next.start();
     } else {
       recipe.finishRecipe();
     }
   }
-
-  DateTime finished;
 
   String get runInterval => started == null
       ? "not started. takes ${formatDuration(time)}"
@@ -109,44 +125,38 @@ class PastRecipe {
 class Recipe {
   static Database db;
 
-  static Future<void> loadDB() async {
-    var dbsPath = await getApplicationDocumentsDirectory();
-    var dbPath = join(dbsPath.path, 'history.db');
-    db = await openDatabase(
-      dbPath,
-      onCreate: (db, version) => db.execute(
-        "CREATE TABLE bakes(ts INTEGER PRIMARY KEY, start INTEGER, recipe TEXT, step INTEGER, note TEXT, state INTEGER)",
-      ),
-      version: 1,
-    );
-  }
+  static Future<void> loadDB() async => db = await openDatabase(
+        join((await getApplicationDocumentsDirectory()).path, 'history.db'),
+        onCreate: (db, version) => db.execute(
+          "CREATE TABLE "
+          "bakes(ts INTEGER PRIMARY KEY, start INTEGER, recipe TEXT, step INTEGER, state INTEGER)",
+        ),
+        version: 1,
+      );
 
   static const int START_STATE = 0;
   static const int FINISH_STATE = 1;
 
   DateTime startTime;
   DateTime finishTime;
-  String recipe = "sourdough";
-  String text;
+  final String recipe;
+  String text; // this should be markdown
 
-  Future<void> logToDB(int step, String note, int state) async {
+  Future<void> logToDB(int step, int state) async {
     var now = DateTime.now();
     var entry = {
       'ts': now.millisecondsSinceEpoch,
       'start': startTime.millisecondsSinceEpoch,
       'recipe': recipe,
       'step': step,
-      'note': note,
       'state': state,
     };
-    db.insert('bakes', entry);
+    await db.insert('bakes', entry);
     /* the timestamp needs to be unique, so wait a bit to ensure that happens */
     sleep(Duration(milliseconds: 2));
   }
 
-  BuildContext buildContext;
-
-  Recipe._();
+  Recipe._(this.recipe);
 
   List<Step> steps;
   int activeStepIndex = 0;
@@ -169,35 +179,32 @@ class Recipe {
       ? steps[activeStepIndex]
       : null;
 
-  static Future<Recipe> startRecipe() async {
-    Recipe recipe = await initRecipe();
+  static Future<Recipe> startRecipe(String recipeName) async {
+    Recipe recipe = await initRecipe(recipeName);
     recipe.startTime = DateTime.now();
-    await recipe.logToDB(-1, "", START_STATE);
+    await recipe.logToDB(-1, START_STATE);
     return recipe;
   }
 
   void finishRecipe() async {
-    print("***** FINISHING RECIPE *****");
-    await logToDB(-1, "", FINISH_STATE);
+    finishTime = DateTime.now();
+    await logToDB(-1, FINISH_STATE);
   }
 
-  static Future<Recipe> initRecipe() async {
-    String text = await rootBundle.loadString("assets/recipes/sourdough.md");
-    Recipe recipe = Recipe._();
+  static Future<Recipe> initRecipe(String recipeName) async {
+    String text =
+        await rootBundle.loadString("assets/recipes/${recipeName}.md");
+    Recipe recipe = Recipe._(recipeName);
     recipe.text = "";
     recipe.steps = List<Step>();
     int index = 0;
-    Step prev = null;
     for (var line in text.split("\n")) {
-      print("processing ${line}");
       if (line.startsWith("=>")) {
         var parts = line.split(" ");
         int mins = int.parse(parts[1]);
         String desc = parts.sublist(2).join(" ");
-        var step = Step(recipe, index, prev, desc, Duration(minutes: mins));
-        prev = step;
+        var step = Step(recipe, index, desc, Duration(minutes: mins));
         recipe.steps.add(step);
-        if (index > 0) recipe.steps[index - 1].next = recipe.steps[index];
         index++;
       } else {
         recipe.text += line + '\n';
@@ -207,17 +214,14 @@ class Recipe {
   }
 
   static Future<Recipe> continueRecipe(DateTime startTime) async {
-    var startTimeMillis = startTime.millisecondsSinceEpoch;
     List<Map> results = await db.query('bakes',
-        columns: ['start', 'step', 'ts', 'state'],
+        columns: ['start', 'step', 'ts', 'state', 'recipe'],
         where: 'start = ?',
         whereArgs: [startTime.millisecondsSinceEpoch]);
-    Recipe recipe = await initRecipe();
+    assert(results.length > 0);
+    Recipe recipe = await initRecipe(results.first['recipe']);
     recipe.startTime = startTime;
-    print("continuing recipe from ${startTimeMillis}");
-    print(results);
     for (var entry in results) {
-      print("Processing $entry");
       if (entry['step'] != -1) {
         DateTime dt = DateTime.fromMillisecondsSinceEpoch(entry['ts']);
         switch (entry['state']) {
@@ -238,8 +242,6 @@ class Recipe {
   }
 
   static Future<List<PastRecipe>> pastRecipes() async {
-    print("loading past recipes");
-    List<PastRecipe> past = List<PastRecipe>();
     List<Map> results = await db.query(
       'bakes',
       columns: ['start', 'recipe', 'ts', 'state', 'step'],
@@ -258,39 +260,39 @@ class Recipe {
         map[start] = pastRecipe;
       }
       if (entry['state'] == FINISH_STATE) {
-        print("setting finished on ${pastRecipe}: $entry");
         pastRecipe.finishedTime = DateTime.fromMillisecondsSinceEpoch(ts);
       }
     }
 
     var keys = map.keys.toList(growable: false);
     keys.sort((a, b) => a - b);
-    for (var key in keys) {
-      past.add(map[key]);
-    }
-    print("returning past");
+    List<PastRecipe> past = List<PastRecipe>();
+    for (var key in keys) past.add(map[key]);
     return past;
   }
 }
 
 class _RecipeStepsState extends State<RecipeSteps> {
+  /* all the async stuff is tracked in this Future. it returns a recipe, but
+   * it is supposed to set the recipe member before it finishes. */
   Future<Recipe> setupFuture;
   Recipe recipe;
+  final String recipeName;
+
+  _RecipeStepsState(this.recipeName);
 
   @override
   void initState() {
-    print("calling initState");
     super.initState();
     Future<Recipe> recipeFuture =
         Recipe.loadDB().then((value) => Recipe.pastRecipes().then((recipes) {
-              print("got recipe: ${recipes}");
               if (recipes.length > 0 && recipes.last.finishedTime == null) {
                 return Recipe.continueRecipe(recipes.last.startTime)
                     .then((recipe) {
                   return recipe;
                 });
               } else {
-                return Recipe.startRecipe();
+                return Recipe.startRecipe(recipeName);
               }
             }));
     setupFuture = recipeFuture
@@ -298,11 +300,8 @@ class _RecipeStepsState extends State<RecipeSteps> {
         .then((recipe) => this.recipe = recipe);
   }
 
-  Duration tickTime = Duration(seconds: 1);
-
-  bool checked = false;
-
   Container constructStepList() {
+    /* make a step tile */
     ListTile makeListTile(Step step, bool active) => ListTile(
           contentPadding:
               EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
@@ -316,7 +315,6 @@ class _RecipeStepsState extends State<RecipeSteps> {
               : Checkbox(
                   value: step.finished != null,
                   onChanged: (value) {
-                    print("step.prev == ${step.prev}");
                     if (value &&
                         (step.prev == null || step.prev.finished != null)) {
                       step.finish();
@@ -336,7 +334,8 @@ class _RecipeStepsState extends State<RecipeSteps> {
                       onPressed: () {
                         scheduleAlarm(
                             step.time.inSeconds, "${step.description}");
-                        setState(() => step.alarmScheduled = false);
+                        step.alarmScheduled = true;
+                        setState(() {});
                       }))
               : null,
         );
@@ -344,25 +343,28 @@ class _RecipeStepsState extends State<RecipeSteps> {
         elevation: 8.0,
         margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
         child: Container(
-          decoration: BoxDecoration(/*color: Colors.amber*/),
+          decoration: BoxDecoration(),
           child: makeListTile(step, active),
         ));
+    /* the list starts with a markdown widgit with the recipe. then all the
+     * steps, finally a button to restart the recipe. that is why there are +2
+     * items. since the first item is not a step. we need to subtract one from
+     * the item index to know the actual (zero-based) step we are on. */
     return Container(
         child: ListView.builder(
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
       itemCount: recipe.steps.length + 2,
       itemBuilder: (BuildContext context, int index) {
-        if (index == 0) {
+        if (index == 0)
           return Markdown(
             data: recipe.text,
             shrinkWrap: true,
           );
-        }
         index--;
         if (index == recipe.steps.length) {
           return RaisedButton(
-              onPressed: () => Recipe.startRecipe().then((r) {
+              onPressed: () => Recipe.startRecipe(recipeName).then((r) {
                     setState(() => this.recipe = r);
                   }),
               child: Text("start new bake"));
@@ -376,15 +378,13 @@ class _RecipeStepsState extends State<RecipeSteps> {
   }
 
   @override
-  Widget build(BuildContext buildContext) {
-    return Scaffold(
-        body: FutureBuilder(
-            future: setupFuture,
-            builder: (BuildContext buildContext, AsyncSnapshot<void> snapshot) {
-              if (recipe == null) {
-                return Text("loading");
-              }
-              return constructStepList();
-            }));
-  }
+  Widget build(BuildContext buildContext) => Scaffold(
+      body: FutureBuilder(
+          future: setupFuture,
+          builder: (BuildContext buildContext, AsyncSnapshot<void> snapshot) {
+            if (recipe == null) {
+              return Text("loading...");
+            }
+            return constructStepList();
+          }));
 }
